adccb449f9298182edd1ceb3e8ef02045d3b5caf
Tim Kraemer <tim022402@gmail.com>
2023-09-18 08:56:37 -0700

    restructuring, created server side and client side respectful directories

--- ws_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This bash script starts the webserver to display sensor data
-
-current_directory=$(pwd)
-cd web_server
-(python3 main.py > stdout) >& stderr &
-cd $current_directory
