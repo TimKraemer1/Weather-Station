84340ac2b0ef5aa153dbf4fa459fa18aa26e66e6
Tim Kraemer <tim022402@gmail.com>
2023-11-20 23:05:53 +0000

    synching

--- Server-side/Backend/main.py
+++ Server-side/Backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, send_file
 from datetime import datetime, timedelta
 import hashlib
 import pandas as pd
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 matplotlib.use('agg')
 
 #creates random sequence for secure data transmission
-#url = hashlib.md5("Walnut&Chestnut".encode('utf-8')).hexdigest()
-url = 'temp_data'
+url = hashlib.md5("Walnut&Chestnut".encode('utf-8')).hexdigest()
+#url = 'temp_data'
 
 num_lines = 0
 
@@ -19,14 +19,18 @@ app = Flask(__name__, template_folder="Templates")
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/' + url)
 def parse_data():
     temperature_data = request.args.get('temp_data')
     temperature_time = datetime.now()
     temperature_time += timedelta(hours=5)
-    temperature_time = temperature_time.strftime('%H:%M:%S')
+    temperature_time = temperature_time.strftime('%l:%M %p')
     try:
-        with open('/Users/timkraemer/Desktop/Weather-Station/Server-side/Backend/tempdata.cs', 'a') as f:
+        with open('Weather-Station/Server-side/Backend/tempdata.csv', 'a') as f:
             f.write('{},{}\n'.format(temperature_time, temperature_data))
             f.close()
     except:
@@ -36,11 +40,9 @@ def parse_data():
 
 @app.route('/get_updated_value')
 def get_updated_value():
-    # Return the updated value to the client
-    with open('Weather-Station/Server-side/Backend/tempdata.csv', 'r') as txt_file:
-        last_line = txt_file.readlines()[-1]
-        updated_temp = last_line + '°F'
-        txt_file.close()
+    df = pd.read_csv('Weather-Station/Server-side/Backend/tempdata.csv')
+    updated_temp = (df['temp'].tail(1)).to_string(index=False)
+    return jsonify({'value': updated_temp})
 
 
 
@@ -48,14 +50,22 @@ def get_updated_value():
 
 @app.route('/temp_graph')
 def get_temp_graph():
-    df = pd.read_csv('/Users/timkraemer/Desktop/Weather-Station/Server-side/Backend/tempdata.csv')
-    plt.plot(df['time'].tail(8640), df['temp'].tail(8640))
+    df = pd.read_csv('Weather-Station/Server-side/Backend/tempdata.csv')
+    plt.plot(df['time'].tail(8640), df['temp'].tail(8640), color='blue')
     plt.xlabel('Time')
     plt.ylabel('Temperature')
     plt.xticks(df['time'].tail(8640), rotation=90)
     plt.tight_layout()
-    plt.savefig('/Users/timkraemer/Desktop/Weather-Station/Server-side/Backend/24hTemp.png')
-    return "Success"
+    plt.locator_params(axis='x', nbins=5)
+    plt.ylim([40, 100])
+    plt.savefig('Weather-Station/Server-side/Backend/static/temp_fig.png')
+    return send_file('static/temp_fig.png', mimetype='image/png')
 
-if __name__ == '__main__':
-   app.run(debug = True)
+
+
+
+
+
+
+
+
